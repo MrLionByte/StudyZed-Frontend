545031d131bda4abbdebc16be680697bd77814f0
Mrlionbyte <fanunaf@gmail.com>
2025-04-03 18:10:04 +0530

    hotfix: resolve production vercel config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,16 +10,9 @@ export default defineConfig({
     },
   },
   server: {
-    host: "0.0.0.0", 
-    port: 5173, 
-    strictPort: true, 
-    cors: {
-      origin: ["https://7a15-202-164-149-48.ngrok-free.app"], 
-      credentials: true, 
-    },
-    allowedHosts: [
-      "7a15-202-164-149-48.ngrok-free.app" // Add the allowed Ngrok URL here
-    ],
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    cors: true,
   }
 })
- 
